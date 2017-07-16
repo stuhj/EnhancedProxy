dca7a4615b963c68e2b667d3aafed08a0232cc4f
Hou <343487184@qq.com>
2017-07-16 14:58:10 +0800

    Lru - safe version

--- cache/LRU.h
+++ cache/LRU.h
@@ -1,98 +1,118 @@
-//vec  ：可分配Node的容器
-//如果新来的key 存在于hashmap中，则将对应Node更新置链表末端
-//如果新来的key不存在于hashmap中，且vec为空，则丢弃链表初始端；若vec不为空，则在链表末端插入对应节点
-#pragma once
-#include <iostream>
+#include <memory>
+#include <list>
 #include <unordered_map>
-#include <vector>
-using namespace std;
-template <typename K, typename V>
+#include <string>
+#include <time.h>
+#include <utility>
+#include <limits.h>
+
+
+typedef std::list<std::pair<std::string, std::string>> PairList;
+
+
 struct Node
 {
-	K key;
-	V data;
-	Node *prev, *next;
+    unsigned long time;
+    PairList::iterator position;
 };
-template <typename K, typename V>
-class LRUCache
+
+
+typedef std::unordered_map<std::string, Node> StringNodeMap;
+class Lru
 {
   public:
-	explicit LRUCache(int capacity)
-	{
-		entries_ = new Node<K, V>[capacity];
-		for (int i = 0; i < capacity; ++i)
-			vec.push_back(entries_ + i);
-		head_ = new Node<K, V>;
-		tail_ = new Node<K, V>;
-		head_->prev = NULL;
-		head_->next = tail_;
-		tail_->prev = head_;
-		tail_->next = NULL;
-	}
-	~LRUCache()
-	{
-		delete head_;
-		delete tail_;
-		delete[] entries_;
-	}
-	V *Put(K *k, V *v)
-	{
-		Node<K, V> *node = hashmap_[*k];
-		if (node)
-		{
-			detach(node);
-			node->data = *v;
-			attach(node);
-		}
-		else
-		{
-			if (vec.empty())
-			{
-				node = head_->next;
-				detach(node);
-				hashmap_.erase(node->key);
-			}
-			else
-			{
-				node = vec.back();
-				vec.pop_back();
-			}
-			node->key = *k;
-			node->data = *v;
-			hashmap_[*k] = node;
-			attach(node);
-		}
-		return NULL;
-	}
+    Lru(size_t size) : maxSize_(size)
+    {
+    }
+    // must have copy constructor.
+    Lru(Lru &lru)
+    {
+        this->maxSize_ = lru.getMaxSize();
+        this->size_ = lru.getSize();
+        this->list_ = lru.getList();
+        PairList::iterator it = list_.begin();
+        while (it != list_.end())
+        {
+            Node node;
+            node.time = time(nullptr);
+            node.position = it;
+            this->map_[it->first] = node;
+            it++;
+        }
+    }
+    Lru &operator=(const Lru &) = delete;
 
-	V *Get(K *k)
-	{
-		Node<K, V> *node = hashmap_[*k];
-		if (node)
-		{
-			detach(node);
-			attach(node);
-			return &node->data;
-		}
-		return NULL;
-	}
-	void detach(Node<K, V> *node)
-	{
-		node->prev->next = node->next;
-		node->next->prev = node->prev;
-	}
-	void attach(Node<K, V> *node)
-	{
-		node->prev = tail_->prev;
-		tail_->prev->next = node;
-		node->next = tail_;
-		tail_->prev = node;
-	}
+    void put(std::string url, std::string content)
+    {
+        if (map_.find(url) == map_.end())
+        {
+            list_.push_back(std::pair<std::string, std::string>(url, content));
+            map_[url].position = (--list_.end());
+            map_[url].time = time(nullptr); //getTime
+            int sizeNow = list_.size();
+            while (sizeNow > maxSize_)
+            {
+                auto it = list_.begin();
+                map_.erase(it->first);
+                list_.erase(it);
+            }
+            return;
+        }
+        map_[url].position->second = content;
+        map_[url].time = time(nullptr); //getTime
+        list_.splice(list_.end(), list_, map_[url].position);
+        size_ = list_.size();
+    }
+
+    std::string get(std::string url)
+    {
+        if (map_.find(url) == map_.end())
+        {
+            return std::string();
+        }
+        if (time(nullptr) - map_[url].time > expiredTime_)
+        {
+            erase(url);
+            return std::string();
+        }
+        list_.splice(list_.end(), list_, map_[url].position);
+        return std::string(map_[url].position->second);
+    }
+
+    void erase(std::string url)
+    {
+        if (map_.find(url) == map_.end())
+        {
+            return;
+        }
+        auto it = map_[url].position;
+        list_.erase(it);
+        map_.erase(url);
+    }
+
+    StringNodeMap &getMap()
+    {
+        return map_;
+    }
+    PairList &getList()
+    {
+        return list_;
+    }
+    size_t getSize()
+    {
+        return size_;
+    }
+    size_t getMaxSize()
+    {
+        return maxSize_;
+    }
 
   private:
-	unordered_map<K, Node<K, V> *> hashmap_;
-	vector<Node<K, V> *> vec;
-	Node<K, V> *head_;
-	Node<K, V> *tail_;
-	Node<K, V> *entries_;
+    size_t maxSize_;
+    //need fix later
+    size_t expiredTime_ = INT_MAX;
+    size_t size_;
+    PairList list_;
+    //StringStringMap map_;
+    StringNodeMap map_;
 };
